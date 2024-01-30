eeb280e6119e8dac337c0af451c9995bfeb811b6
Kartik-Ranaut <ranautk6@gmail.com>
2024-01-30 22:46:35 +0530

    all files added

---
+++ src/Components/Card.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Nature from './Nature'
+import Author from './Author'
+export default function Card(props) {
+  function blogClickHandler(){
+    props.blogclick(props.id);
+  }
+  return (
+    <div className='card' onClick={blogClickHandler}>
+        <img className='cardimage' src={props.image}></img>
+        <Nature nature={props.nature}></Nature>
+        <h1 className='titleofcard'>{props.title}</h1>
+        <Author Date={props.Date}></Author>
+    </div>
+  )
+}
